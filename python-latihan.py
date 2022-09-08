a86a58c6ff11e813c18e5def8ca43e0cd266c36d
krisnabayupangestu <112367852+krisnabayupangestu@users.noreply.github.com>
2022-09-08 13:40:21 +0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,9 @@
+#Rumus segitiga sama kaki sisi + sisi + sisi 
+
+tinggi = int(input("masukkan nilai tinggi: "))
+alas = int(input("masukkan nilai alas: "))
+luas_SSK = 1/2 x alas x tinggi
+
+print("luas segitiga sama kaki adalah", luas_SSK
+
+#rumus j      
